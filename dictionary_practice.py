e6b1b391d17f451232e55d788c38fcca2be086db
tedgmeet <tedg@meet.mit.edu>
2017-03-05 08:27:51 -0500

    Adding resources for lab

---
+++ dictionary_practice.py
@@ -0,0 +1,39 @@
+#Module 6, Part 5: Practice with dictionaries here
+
+
+my_phonebook={"Statue of Liberty":2125555555, "Ghostbusters":2125551234}
+
+###
+#Assign the value of the Statue of Liberty's phone number to the variable, num
+###
+
+###
+#Print the variable, num
+###
+
+###
+#Change the value of the Ghostbusters' phone number to 2125559876
+###
+
+#Here's an empty dictionary
+your_phonebook={}
+
+###
+#Add 5 values to it like this
+#your_phonebook['key']=value
+###
+
+###
+#Use the keys() method: your_phonebook.keys()
+#It will produce a sequence of all the keys
+
+#Loop over this sequence with a for loop
+#using syntax like
+
+#for key in sequence_of_keys :
+#    #Do stuff
+
+#Use the loop to print to the shell the key and value
+#of every element in the dictionary
+###
+
